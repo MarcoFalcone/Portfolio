e8781cdfad00e75ab71eac1839bb9a0466b4b050
Markrius <marco.falcone@hotmail.it>
2020-11-28 19:28:56 +0100

    changed js script

--- static/main.js
+++ static/main.js
@@ -1,1 +1,38 @@
-const scroll = new SmoothScroll('a[href*="#"]');
+//hideanchorlinkurl and smoothscrooll//
+$('.backhome').click(function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $("#homedevicecont").offset().top - 0
+    }, 600);
+});
+
+$('.aboutdevice').click(function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $("#aboutdevicecont").offset().top - 0
+    }, 600);
+});
+
+$('.worksdevice').click(function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $("#workdevicecont").offset().top - 0
+    }, 600);
+});
+
+$('.contactdevice').click(function (e) {
+    e.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $("#contactdevicecont").offset().top - 0
+    }, 600);
+});
+
+//prevent the softkeyboard to change layout on android//
+var meta = document.createElement('meta');
+meta.name = 'viewport';
+meta.content = 'width=device-width,height='+window.innerHeight+', initial-scale=1.0';
+document.getElementsByTagName('head')[0].appendChild(meta);
