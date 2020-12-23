a3dd63854a42d5a402afa95f67b8c46f6b2b8376
Markrius <marco.falcone@hotmail.it>
2020-12-23 19:42:38 +0100

    update contact

--- server.py
+++ server.py
@@ -1,28 +1,10 @@
-from flask import Flask, render_template, request
-import smtplib
+from flask import Flask, render_template,
 
 app = Flask(__name__)
 
 @app.route('/')
 def page():
     return render_template('index.html')
-
-@app.route('/submit_form', methods=['POST'])
-def submit_form():
-        name = request.form['name']
-        email = request.form['email']
-        msg = request.form['message']
-
-        user = "email"
-        password = "password"
-        message = str(name) + '\n\n' + str(email) + '\n\n' + str(msg)
-        server = smtplib.SMTP("smtp.gmail.com:587")
-        server.starttls()
-        server.login(user, password)
-        server.sendmail(user, user, message)
-        server.quit()
-
-        return render_template('thankyou.html')
 
 @app.errorhandler(500)
 def internal_server_error(e):
