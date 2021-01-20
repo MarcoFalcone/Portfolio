bbd8b4337f05e962adb5d150a2721806cea3e64c
Markrius <marco.falcone@hotmail.it>
2021-01-20 01:36:43 +0100

    cleaned code

--- static/main.js
+++ static/main.js
@@ -33,34 +33,36 @@ function showSlides(n) {
 }
 
 //hideanchorlinkurl and smoothscrooll//
-$('.backhome').click(function (e) {
+$(document).ready(function(){
+  $('.backhome').click(function (e) {
     e.preventDefault();
 
     $('.wrap').animate({
-        scrollTop: $("#homedevicecont").offset().top - 0
+      scrollTop: $("#homedevicecont").offset().top - 0
     }, 600);
-});
+  });
 
-$('.aboutdevice').click(function (e) {
+  $('.aboutdevice').click(function (e) {
     e.preventDefault();
 
     $('.wrap').animate({
-        scrollTop: $("#aboutdevicecont").offset().top - 0
+      scrollTop: $("#aboutdevicecont").offset().top - 0
     }, 600);
-});
+  });
 
-$('.worksdevice').click(function (e) {
+  $('.worksdevice').click(function (e) {
     e.preventDefault();
 
     $('.wrap').animate({
-        scrollTop: $("#workdevicecont").offset().top - 0
+      scrollTop: $("#workdevicecont").offset().top - 0
     }, 600);
-});
+  });
 
-$('.contactdevice').click(function (e) {
+  $('.contactdevice').click(function (e) {
     e.preventDefault();
 
     $('.wrap').animate({
-        scrollTop: $("#contactdevicecont").offset().top - 0
+      scrollTop: $("#contactdevicecont").offset().top - 0
     }, 600);
+  });
 });
